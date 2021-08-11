266ba8f357f59685ac992c3f4759c0b1f7ccf312
Joachim Dalen <me@d3v.no>
2021-08-11 16:29:35 +0200

    Add samples

---
+++ sample/migrations/seeders/20210811_141853_seed_products.sql
@@ -0,0 +1,6 @@
+INSERT INTO
+    "public"."PRODUCTS" ("PRODUCT_ID", "NAME")
+VALUES
+    ('f6b624b6-7db8-4883-877c-2b66ce58d15d', 'Apples'),
+    ('ea6f55bb-4827-413b-b1ad-e95e5c0a9b8a', 'Grapes'),
+    ('941f7f79-390e-454e-8f6c-f9953a2d4895', 'Flour');
